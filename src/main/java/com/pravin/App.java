7d492603d97b391347416918552f86e897ebd540
Pravin <gawarepravinraje@gmail.com>
2022-07-13 09:11:24 +0530

    first commit

---
+++ src/main/java/com/pravin/App.java
@@ -0,0 +1,13 @@
+package com.pravin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
